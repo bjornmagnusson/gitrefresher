de7e2ae45c4b32be4854a9facadf451ee8f3c051
Björn Magnusson <bjorn.magnusson@teliacompany.com>
2022-06-14 15:55:19 +0200

    Lookup current branch with git command

--- gitrefresh.sh
+++ gitrefresh.sh
@@ -4,7 +4,7 @@ echo "Found repositories, $(dirname $REPOSITORIES_DOT_GIT)"
 for repo in $REPOSITORIES_DOT_GIT; do
     cd $(dirname $FETCH_DIR/$repo)
     echo "Updating repository $repo"
-    current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+    current_branch=$(git branch --show-current)
     git fetch --all --prune
     for branch in develop master $current_branch; do
         git co $branch
